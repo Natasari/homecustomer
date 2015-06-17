ddd96f497a9ae97d54bc807ab1d7f34de7e65b69
leli maharani <maharanileli@gmail.com>
2015-06-17 13:33:39 +0700

    baru

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -17,13 +17,31 @@ class admin extends CI_Controller {
 		$this->load->view('tambah_admin');
 
 	}
-	public function insert(){
+	public function insert()
+	{
 		session_start();
-		$this->load->view('tambah_admin');
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$this->load->model('login_model');
-		$result = $this->login_model->insert_user($username, $password);
+		if($_SERVER['REQUEST_METHOD']== 'POST')
+		{
+			$this->form_validation->set_rules('username','Username','required');
+			$this->form_validation->set_rules('password', 'Password', 'required');
+			$this->form_validation->set_message('required', '%s must be filled');
+			if($this->form_validation->run() == TRUE)
+			{
+				$username = $this->input->post('username');
+				$password = $this->input->post('password');
+				//echo $username;
+				//echo $password;
+				$this->load->model('admin_model');
+				$result = $this->admin_model->insert_user($username, $password);
+			}
+			else{
+					$this->load->view('tambah_admin');
+					echo '<script>alert("username and password must be filled");</script>'; 
+				}
+		}
+		else{
+				$this->load->view('tambah_admin');
+			}	
 	}
 }
 ?>
--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -4,33 +4,11 @@
 		function __construct()
 		{
 			parent::__construct();
-			
 		}
-		public function tambah_user($username, $password) {
-			//to give return function that wrong username or password
-			$nothing=0;
-			//load database from localhost : 127.0.0.1
-			$this->load->database(); 
-			//get hash password from database
-			$hashuserpassword = $this->db->query("SELECT USERS.PASS FROM USERS WHERE USERS.USERNAME='$username'");
-			$result = $hashuserpassword->result_array();
-			$hash = $result[0]['PASS'];
-
-			//to verify that password and hash password match
-			if (password_verify($password, $hash)) {
-				$query = $this->db->query("SELECT USERS.ID_USERS, USERS.PREV, USERS.USERNAME FROM USERS WHERE USERS.USERNAME='$username' AND USERS.PASS='$hash'");
-		    	$result = $query->result_array();
-		    	$data = array(
-		    		'USERNAME' => $result[0]['USERNAME'],
-		    		'ID_USERS' => $result[0]['ID_USERS'],
-		    		'PREV' => $result[0]['PREV']
-		    	);
-		    	session_start();
-		    	return $data;
-			}
-			else {
-			    return $nothing;
-			}
+		public function insert_user($username,$password) {
+				$this->load->database(); 
+				$result = $this->db->query("INSERT INTO USERS (USERS.USERNaME, USERS.PASS) VALUES ('$username','$password')");
+				//print_r($result);
 		}
 	}
 
